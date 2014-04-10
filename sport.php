7d9e16600d3070fddce0062b5ab0160c860eefc6
KailasDev <kailas3476@gmail.com>
2014-04-11 01:03:33 +0200

    Ajout de la session

--- sport.php
+++ sport.php
@@ -1,40 +1,41 @@
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-  <head>
+<head>
     <title>sans titre</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
     <meta name="generator" content="Geany 1.23.1" />
     <link rel="stylesheet" type="text/CSS" href="planete.css"/>
-  </head>
+</head>
 
-  <body>
-    <?php include('titre.php'); ?>
-    <?php include('menu.php'); ?>
-    
-    <div id ="CORPS">
-            
-      <?php
-      echo "<h3>QCM Sport</h3>";
-      if(file_exists("sport.txt")) {
+<body>
+<?php include('titre.php'); ?>
+<?php include('menu.php'); ?>
+
+<div id ="CORPS">
+
+    <?php
+    echo "<h3>QCM Sport</h3>";
+    if(file_exists("sport.txt")) {
         $fichier = fopen("sport.txt", "r");
         $nbre = 1;
-          
-        while($nbre < 5) {    
-          $question = fgets($fichier);
-          echo "<p>Question ".$nbre.":</p>";
-          echo $question;
-          ++$nbre;
+
+        while($nbre < 5) {
+            $question = fgets($fichier);
+            echo "<p>Question ".$nbre.":</p>";
+            echo $question;
+            ++$nbre;
         }
         fclose($fichier);
-        }
-      ?>
-      
-      
-    </div>
-    
-    <?php include('pied.php'); ?>    
-  </body>
+    }
+    ?>
+
+
+</div>
+
+<?php include('pied.php'); ?>
+</body>
 
 </html>
