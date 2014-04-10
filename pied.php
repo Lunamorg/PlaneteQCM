7f15d0c7189a670e6fd119a50d95687424e2d066
KailasDev <kailas3476@gmail.com>
2014-04-11 01:01:53 +0200

    Ajout du nombre de connecté

--- pied.php
+++ pied.php
@@ -1,12 +1,20 @@
-
 <div id ="PIED_PAGE">
-  <table>
-    <tr>
-      <td><a href="planteteqcm@noreply.com">Contact</a></td>    
-      <?php 
-        $date= date("D d F Y");
-        echo "<td>".($date)."</td>";
-       ?>   
-    </tr>
-  </table>
+    <table id="pied_tableau">
+        <tr>
+            <td><?php
+                include_once("fonction.php");
+                connexion("projet");
+                echo nbConnecte("planeteqcm", array("connecte" => "true"))." connecte(s)";
+                deconnexion();
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td><a href="planteteqcm@noreply.com">Contact</a></td>
+            <?php
+            $date= date("D d F Y");
+            echo "<td>".($date)."</td>";
+            ?>
+        </tr>
+    </table>
 </div>
