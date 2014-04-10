1ca7c94ff82c36ce647351c2ea7d5edf8e8f2aea
KailasDev <kailas3476@gmail.com>
2014-04-11 00:58:56 +0200

    Delete inscrit.php

--- inscrit.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once('fonction.php');
-  if(estCorrect(array($_POST['pseudo'], $_POST['mdp'], $_POST['email']))) {
-    connexion('projet');
-    selectionner('planeteqcm', array($_POST['pseudo'], $_POST['mdp'], $_POST['email']));
-    deconnexion();
-  }
-  header('location: ajouter.php?id='.$_POST['pseudo']);
-?>
