00825ad8c9e7ef524654b755a2764ba3d30d7fc6
KailasDev <kailas3476@gmail.com>
2014-04-11 00:59:43 +0200

    Ajout du systeme connecté

--- menu.php
+++ menu.php
@@ -1,14 +1,19 @@
-
 <div id="MENU">
-  <table>
-    <thead> 
-    <tr>
-      <td><a href="planete.php"     title="accueil">Accueil</a></td>
-      <td><a href="formulaire.php"  title="inscription">Inscription </a></td>
-      <td><a href="connection.php"  title="membre">Membres </a></td>
-      <td><a href="profil.php"      title="profil">Profil</a></td>      
-    </tr>
-    </thead>
-  </table>
-</div>
+    <table id="menu_tableau">
+        <thead>
+        <tr>
+            <td><a href="planete.php"     title="accueil">Accueil</a></td>
+            <?php
+                if(!isset($_SESSION['pseudo'])){
+                    echo '<td><a href="formulaire.php"  title="inscription">Inscription</a></td>';
+                    echo '<td><a href="connection.php"  title="membre">Connexion</a></td>';
+                }
+                else
+                    echo '<td><a href="traitement.php?type=deconnexion"  title="deconnexion">deconnexion</a></td>';
+            ?>
 
+            <td><a href="profil.php" title="profil">Profil</a></td>
+        </tr>
+        </thead>
+    </table>
+</div>
