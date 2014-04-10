47f9eabf83a416704bd4be598320fc589ceaeaaa
KailasDev <kailas3476@gmail.com>
2014-04-11 00:58:31 +0200

    Regroupement, ajout de fonctions
    Regroupement en un seul fichier, traitement de connexion, inscription, deconnexion, gestion des sessions, gestion du nombre de connecté

--- traitement.php
+++ traitement.php
@@ -1,10 +1,42 @@
 <?php
-  include_once('fonction.php');
-  if(id == inscription)
-  if(estCorrect(array($_POST['pseudo'], $_POST['mdp'], $_POST['email']))) {
-    connexion('projet');
-    ajouter('planeteqcm', array($_POST['pseudo'], $_POST['mdp'], $_POST['email']));
-    deconnexion();
-  }
-  header('location: ajouter.php?id='.$_POST['pseudo']);
+session_start();
+include_once('fonction.php');
+
+if(estCorrect($_GET['type']))
+{
+    if($_GET['type'] == 'inscription')
+    {
+        if(estCorrect(array($_POST['pseudo'], $_POST['mdp'], $_POST['email']))) {
+            connexion('projet');
+            ajouter('planeteqcm', array($_POST['pseudo'], $_POST['mdp'], $_POST['email'], '0'));
+            deconnexion();
+            echo "enregistre";
+        }
+    }
+    else if($_GET['type'] == 'connexion')
+    {
+        connexion('projet');
+        $resultat = selectionner('planeteqcm', array('mdp'), array("pseudo" => $_POST['pseudo']));
+        deconnexion();
+        if($resultat['mdp'] == $_POST['mdp'])
+        {
+            $_SESSION['pseudo'] = $_POST['pseudo'];
+            connexion('projet');
+            maj("planeteqcm", array("connecte" => "true"), array("pseudo" => $_POST['pseudo']));
+            deconnexion();
+        }
+    }
+    else if($_GET['type'] == 'deconnexion')
+    {
+        connexion('projet');
+        maj("planeteqcm", array("connecte" => "false"), array("pseudo" => $_SESSION['pseudo']));
+        deconnexion();
+        session_destroy();
+    }
+}
+else
+{
+    echo "page incorrect";
+}
+header('location: planete.php');
 ?>
