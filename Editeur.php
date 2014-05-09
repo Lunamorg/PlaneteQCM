c2d0a5306881b70e704fd6f13afd09f1238f9d6f
kailas3476@gmail.com <kailas3476@gmail.com>
2014-05-09 18:06:42 +0200

    Réduction du php

--- Editeur.php
+++ Editeur.php
@@ -39,91 +39,18 @@
             <input type='submit' value='Cr&eacute;er'/>            
           </form>";
         } else if($_GET['type'] == 'sup') {
-          unlink("qcm/" . $_GET['matiere'] . "/qcm.txt");
-          rmdir("qcm/" . $_GET['matiere']);
-          unlink("images/". $_GET['matiere'] .".png");
+          supprimer_editeur($_GET['matiere']);
         } else if($_GET['type'] == 'add') {
-          mkdir("qcm/" . $_POST['matiere']);
-          $fichier = fopen("qcm/" . $_POST['matiere'] . "/qcm.txt", "w");
-          fclose($fichier);
-          $chemin = 'images/';
-          move_uploaded_file($_FILES['nom_image']['tmp_name'],$chemin.$_POST['matiere'].".png");
+          ajouter_editeur($_POST['matiere']);
         } else if($_GET['type'] == 'mod') {
-
-          $i = 0;
-          $j = 0;
-          $reponse = array();
-          $score = 0;
-
-          foreach(lireQCM("qcm/" . $_GET['matiere'] . "/qcm.txt", false) as $valeur) {
-            if($i % 3 == 2) {
-              $reponse[$j] = $valeur;
-              ++$j;
-            }
-            ++$i;
-          }
-
-          echo '<form method="post" action="editeur.php?type=sav&amp;matiere=' . $_GET['matiere'] . '"><div class="qr">';
-          $i = 0;
-          $j = 0;
-          $k = 0;
-          foreach(lireQCM("qcm/" . $_GET['matiere'] . "/qcm.txt", false) as $valeur) {
-
-            if($i % 3 == 0) {
-              echo '<div class="qr"><label>Question ' . (floor($i / 3)+1) . ' : </label> <input type="text" name="question' . (floor($i / 3)) . '" value=' . str_espace($valeur) . ' /><a class="a_qcm" href="editeur.php?type=supquestion&matiere=' . $_GET['matiere'] . '&question=' . (floor($i / 3)+1) . '">Supprimer</a><br/>';
-            } else if($i % 3 == 1) {
-              $tab = explode("$", $valeur);
-              $j = 0;
-              foreach($tab as $val) {
-                ++$j;
-                $t = "";
-                if($j == $reponse[$k])
-                  $t = 'checked="checked"';
-                echo '<input type="radio" name="rep' . (floor($i / 3)) . '" value="' . $j . '" ' . $t . '/>' . '<input type="text" name="pro' . (floor($j)) . '-' . (floor($i / 3)) . '" value=' . str_espace($val) . ' />' . '<br/>';
-              }
-              ++$k;
-              echo '</div><hr/>';
-            }
-            ++$i;
-          }
-          echo '<input type="submit" value="Soumettre"/>';
-          echo '</div></form>';
-          echo '<form method="post" action="editeur.php?type=savun&amp;matiere=' . $_GET['matiere'] . '">';
-          echo '<div>';
-          echo '<label>Question ' . (floor($i / 3)+1) . ' : </label><input type="text" name="question"/><br/>';
-          echo '<input type="radio" name="n_choix" value="1"/><input type="text" name="pro1"/><br/>';
-          echo '<input type="radio" name="n_choix" value="2"/><input type="text" name="pro2"/><br/>';
-          echo '<input type="radio" name="n_choix" value="3"/><input type="text" name="pro3"/><br/>';
-          echo '<input type="radio" name="n_choix" value="4"/><input type="text" name="pro4"/><br/>';
-          echo '<input type="submit" value="Ajouter"/>';
-          echo '</div></form>';
+          /**
+           * Du html est ecrit dans cet fonction
+           */
+          modifier_editeur($_GET['matiere']);
         } else if($_GET['type'] == 'sav') {
-          $taille = count(lireQCM("qcm/" . $_GET['matiere'] . "/qcm.txt", false)) / 3;
-          effacerFichier("qcm/" . $_GET['matiere'] . "/qcm.txt");
-          for($j = 0; $j < $taille; ++$j) {
-            $reponse = array();
-            $i = 1;
-            echo $_POST['question' . ($j)] . '<br/>';
-            while(isset($_POST['pro' . $i . '-' . $j])) {
-              $reponse[$i - 1] = $_POST['pro' . $i . '-' . $j];
-              echo $reponse[$i - 1] . "<br/>";
-              ++$i;
-            }
-            echo $_POST['rep' . ($j)] . '<br/>';
-            sauvegarde("qcm/" . $_GET['matiere'] . "/qcm.txt", $_POST['question' . ($j)], $reponse, $_POST['rep' . ($j)]);
-          }
+          sauvegarderPlusieurs_editeur($_GET[matiere], $_POST);
         } else if($_GET['type'] == 'savun') {
-          $reponse = array();
-          $i = 1;
-          echo $_POST['question'] . '<br/>';
-          while(isset($_POST['pro' . $i])) {
-            $reponse[$i - 1] = $_POST['pro' . $i];
-            echo $reponse[$i - 1] . "<br/>";
-            ++$i;
-          }
-          $reponse[$i-2] .= "\n";
-          echo $_POST['n_choix'] . '<br/>';
-          sauvegarde("qcm/" . $_GET['matiere'] . "/qcm.txt", $_POST['question'] . "\n", $reponse, $_POST['n_choix'] . "\n");
+          sauvegarderUn_editeur($_GET['matiere'], $_POST);
         }
         else if($_GET['type'] == 'supquestion')
         {
